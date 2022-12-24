bd9dfa84a8a0aee14a75543cce5f174f6b3e9023
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-24 15:57:43 +0100

    9

---
+++ main.js
@@ -0,0 +1,11 @@
+// const details = document.querySelector('details')
+// const summary = document.querySelector('summary')
+// const douImage = document.getElementsByClassName('dou')
+// douImage.style.position = 'fixed'
+
+// const nav = document.querySelector('nav') 
+
+// summary.addEventListener(
+//     'click',
+//     nav.style.height = '25rem'
+// )
